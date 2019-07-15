926d7249e0174ce14e3268332d71dc8d3429b53e
MJ0515 <db2736@naver.com>
2019-07-15 17:45:56 +0900

    190715 수업최종

--- basic/test.py
+++ basic/test.py
@@ -3,3 +3,9 @@ print("hi")
 print('''hi
         hi
         hi''')
+food="python's favorite\n food is perl"
+print(food[5:])
+a=6.66666
+print('i eat %0.6f apples.'%a)
+print("python's {1}favo{0}rite".format(12,20))
+
