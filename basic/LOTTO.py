604f3adb91ca7468d9f03556f6e8627a496a9a7e
MJ0515 <db2736@naver.com>
2019-07-16 17:56:08 +0900

    190716 로또만들기 과제

---
+++ basic/LOTTO.py
@@ -0,0 +1,10 @@
+#1-45사이의 번호 6개를 추출
+#총 다섯번에 걸쳐 번호를 추출 정렬하여 출력
+#random함수, rnadint 이용
+#sorted()내장함수이용
+import random
+list1 = []
+
+for i in (0,6):
+    j = [random.randint(1,45)
+    list1.append(j)
